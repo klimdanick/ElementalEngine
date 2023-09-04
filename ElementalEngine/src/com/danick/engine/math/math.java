8719fe7fbf8bb139aa9b6d8c239ddcabf11bdc64
klimdanick <klimdanick@gmail.com>
2023-09-04 11:10:34 +0200

    Delete ElementalEngine directory

--- ElementalEngine/src/com/danick/engine/math/math.java
+++
@@ -1,7 +0,0 @@
-package com.danick.engine.math;
-
-public class math {
-	public double distance(double x1, double y1, double x2, double y2) {
-		return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-	}
-}
