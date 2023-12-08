7a0f94e0cc503fd7076a6912cd15ece2b6901f21
klimdanick <github@klimdanick.nl>
2023-12-08 14:06:49 +0100

    added isometric test with z buffer

--- test/test/test.java
+++ test/test/test.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
@@ -11,44 +12,45 @@ import com.danick.e2.renderer.Graphic3D.Model3D;
 import com.danick.e2.renderer.Renderer3D;
 
 public class test extends AbstractGame{
-
-	Renderer3D r3d;
-	double x = 0;
-	ArrayList<Model3D> cubes = new ArrayList<>();
-	
+	GameContainer gc;
+	double x, y;
 	public static void main(String[] args) {
 		GameContainer gc = new GameContainer(new test(), 600, 400, 1, "test");
 		gc.start();
 	}
 
 	public void init(GameContainer gc, Graphic r) {
-		r3d=new Renderer3D(gc);
-		gc.changeRenderer(r3d);
-		int size = 50;
-		for (double x = -14; x < 15; x++) for (double y = -14; y < 15; y++) {
-			Model3D c = Model3D.hexagon(size+6);
-			double offset = x%2==0 ? 0 : 0.5;
-			c.TranslateVerts((5.0/6.0)*x*size, y*size+offset*size, 0);
-			c.RotateOrigin(0.95, 0, 0);
-			cubes.add(c);
-		}
-		
+		r.bgColor = Color.red.darker();
+		this.gc = gc;
 	}
 	
 	public void update(GameContainer gc, long dt) {
-		
+		if (gc.input.isKey(KeyEvent.VK_W)) y+=0.1;
+		if (gc.input.isKey(KeyEvent.VK_S)) y-=0.1;
+		if (gc.input.isKey(KeyEvent.VK_A)) x+=0.1;
+		if (gc.input.isKey(KeyEvent.VK_D)) x-=0.1;
 	}
 	
 	public void render(GameContainer gc, Graphic r) {
-		r3d.projection = new OrthographicProjection();
-		r3d.clear();
-		
-		for (Model3D cube : cubes) r3d.drawModel(cube);
-		
-		if (gc.input.isKey(KeyEvent.VK_E)) for (Model3D cube : cubes) cube.RotateOrigin(0, 0, Math.PI/80);
-		if (gc.input.isKey(KeyEvent.VK_Q)) for (Model3D cube : cubes) cube.RotateOrigin(0, 0, -Math.PI/80);
-		//if (gc.input.isKey(KeyEvent.VK_R)) for (Model3D cube : cubes) cube.RotateOrigin(Math.PI/160, 0, 0);
-		//if (gc.input.isKey(KeyEvent.VK_F)) for (Model3D cube : cubes) cube.RotateOrigin(-Math.PI/160, 0, 0);
+		r.clear();
+		double z = Math.sin((double)x/2) + Math.sin((double)y/2)+1;
+		int[] uv = project(x, y, z);
+		r.drawCircle(uv[0], uv[1], y-x-z, 20, Color.red, true, 100);
+		Graphic[] tiles = new Graphic[] {Graphic.fromImage("tileSet/grass_half.png")};
+		for (int i = -50; i < 40; i++) for (int j = 40; j >= -50; j--) {
+			z = Math.sin((double)i/2) + Math.sin((double)j/2);
+			uv = project(i, j, z);
+			r.drawGraphic(tiles[0], uv[0]-tiles[0].pixelWidth/2, uv[1]-tiles[0].pixelHeight/2, j-i-z);
+		}
+		r.drawGraphic(r.getZBuffer(), 0, 0, Integer.MIN_VALUE);
 	}
 
+	public int[] project(double x, double y, double z) {
+		int u, v;
+		u = (int) Math.round(16 * x + 16 * y);
+		v = (int) Math.round(8 * x - 8 * y - 16 * z);
+		u += gc.width / 2;
+		v += gc.height;
+		return new int[] { u, v };
+	}
 }
