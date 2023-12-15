12210e407e77ece7d2c32d004b2f0cb26f1455fc
klimdanick <github@klimdanick.nl>
2023-12-15 15:50:19 +0100

    merged client and server to 1 class

--- test/test/client.java
+++ test/test/client.java
@@ -4,28 +4,33 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class client {
-	DatagramSocket ds;
+	DatagramSocket recDS;
+	DatagramSocket senDS;
 	InetAddress adress;
 	byte buf[] = new byte[900];
 	Thread senThread;
 	Thread recThread;
 	
-	Queue<Message> msgs = new LinkedList<>(); 
-	
-	public client(String ip, int port) throws IOException {
-		ds = new DatagramSocket();
-		InetAddress adress = InetAddress.getByName(ip);
-		this.adress = adress;
+	public static HashMap<Byte, Message> recMsgs = new HashMap<>();
+	Queue<Message> senMsgs = new LinkedList<>(); 
+	public client(String ip, int recPort, int senPort) throws IOException {
+		this(recPort, senPort);
+		this.adress = InetAddress.getByName(ip);
+	}
+	public client(int recPort, int senPort) throws IOException {
+		senDS = new DatagramSocket();
 		this.senThread = new Thread() {
 			public void run() {
 				while(true) {
 					Message msg;
-					while ((msg = msgs.poll()) == null)
+					while ((msg = senMsgs.poll()) == null)
 						try {
 							Thread.sleep(10);
 						} catch (InterruptedException e) {
@@ -43,53 +48,115 @@ public class client {
 					}
 					
 					
-					DatagramPacket DpSend = new DatagramPacket(buffer, buffer.length, adress, port); 
+					DatagramPacket DpSend = new DatagramPacket(buffer, buffer.length, adress, senPort); 
 
 					try {
-						ds.send(DpSend);
+						senDS.send(DpSend);
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 					
-					if (msg.index >= msg.data.length) msg = null;
+					if (msg.index >= msg.data.length) {
+						System.out.println("SEND: " + new String(msg.data));
+						msg = null;
+					}
 					else msg.part++;
 				}
 			}
 		};
-		senThread.start();
+		
 		
 		this.recThread = new Thread() {
 			public void run() {
-				while(true) {
-					
+				try {
+					recDS = new DatagramSocket(recPort);
+					byte[] receive = new byte[65535];
+	
+					DatagramPacket DpReceive = null;
+					while(true) {
+	
+						// Step 2 : create a DatgramPacket to receive the data.
+						DpReceive = new DatagramPacket(receive, receive.length);
+	
+						// Step 3 : revieve the data in byte buffer.
+						try {
+							recDS.receive(DpReceive);
+							Message m = data(receive);
+							if (m.part == receive[3]) System.out.println("RESC: " + m);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+	
+						// Clear the buffer after every message.
+						receive = new byte[65535];
+					}
+				} catch (SocketException e) {
+					e.printStackTrace();
 				}
 			}
 		};
+	}
+	
+	public void start() {
+		senThread.start();
 		recThread.start();
 	}
 	
 	public Message sendMessage(int type, byte[] data) {
 		Message m = new Message((byte)type, data);
-		msgs.add(m);
+		senMsgs.add(m);
 		return m;
 	}
 	
-	public static client connect(String ip, int port) {
+	public static client startClient(String ip, int senPort, int recPort) {
 		client c = null;
 		while (c == null) try {
-			c = new client(ip, port);
+			c = new client(ip, senPort, recPort);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		c.start();
 		return c;
 	}
 	
+	public static client startServer(int senPort, int recPort) {
+		client c = null;
+		while (c == null) try {
+			c = new client(senPort, recPort);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		c.start();
+		return c;
+	}
+	
+	public static Message data(byte[] a) {
+		if (a == null)
+			return null;
+		Message m = recMsgs.get(a[0]);
+		if (m == null) {
+			//System.out.println("NEW MESSAGE!");
+			m = new Message(a[0]);
+			recMsgs.put(a[0], m);
+			m.part = a[1];
+			m.type = a[2];
+			m.data = new byte[a[3]*Message.MAX_BUFFER_SIZE];
+			//System.out.println(m);
+		}
+		m.part = a[1];
+		for (int i = 0; i < Message.MAX_BUFFER_SIZE && i < a.length; i++) {
+			m.data[i+((m.part-1) * Message.MAX_BUFFER_SIZE)] = a[i+Message.HEADER_SIZE];
+		}
+		if (m.part == a[3]) recMsgs.put(a[0], null);
+		return m;
+		
+	}
 	
 	public static void main(String[] args) throws IOException {
 		Thread t = new Thread() {
 			public void run() {
 				Scanner sc = new Scanner(System.in); 
-				client c = client.connect("localhost", 1234);
+				client c = client.startClient("localhost", 1233, 1234);
 				while(true) {
 					String s = sc.nextLine();
 					c.sendMessage((byte)1, s.getBytes());
