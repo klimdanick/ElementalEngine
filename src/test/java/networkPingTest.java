87a40a8f674aa8976354267d7e4a49a1496ba1fa
klimdanick <github@klimdanick.nl>
2024-01-12 12:45:59 +0100

    unitTest>: test class for networking | added test class for networking unit tests

---
+++ src/test/java/networkPingTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class networkingTests {
+
+	@Test
+	void portSetupTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void pingTest() {
+		fail("Not yet implemented");
+	}
+
+}
