e34c127cfef003aa107364729b5aa237f74303ef
klimdanick <klimdanick@gmail.com>
2024-02-02 03:08:20 +0100

    Revert "removed dom"
    This reverts commit e439669d7e7b94224f364c5ea5f7703e6cad22d5.

---
+++ src/test/java/MarioDemo/Block.java
@@ -0,0 +1,36 @@
+package MarioDemo;
+
+import java.awt.Color;
+
+import com.danick.e2.main.GameContainer;
+import com.danick.e2.objects.GameObject;
+import com.danick.e2.renderer.Graphic;
+
+public class Block extends GameObject {
+	
+	Item HoldItem;
+	
+	public Block(int x, int y) {
+		super(x*10, y*10, 10, 10);
+	}
+	
+	public Block(int x, int y, Item HoldItem) {
+		super(x*10, y*10, 10, 10);
+		this.HoldItem = HoldItem;
+	}
+
+	@Override
+	public void update(GameContainer gc) {
+		if (collide(0, 1) instanceof Player) hit();
+	}
+
+	@Override
+	public void render(Graphic graphic) {
+		graphic.drawRectangle(0, 0, width, height, 0, new Color(0xff7b3812));
+	}
+
+	
+	public void hit() {
+		gc.removeObject(this);
+	}
+}
---
+++ src/test/java/MarioDemo/Player.java
@@ -0,0 +1,55 @@
+package MarioDemo;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+
+import com.danick.e2.main.GameContainer;
+import com.danick.e2.objects.GameObject;
+import com.danick.e2.renderer.E2Color;
+import com.danick.e2.renderer.Graphic;
+
+public class Player extends GameObject {
+	
+	double ySpd = 0;
+
+	public Player(int x, int y) {
+		super(x*10, y*10, 10, 10);
+	}
+
+	@Override
+	public void update(GameContainer gc) {
+		double yMove = 0;
+		double xMove = 0;
+		if (gc.input.isKey(KeyEvent.VK_D)) xMove+=0.7;
+		if (gc.input.isKey(KeyEvent.VK_A)) xMove-=0.7;
+		if ((gc.input.isKey(KeyEvent.VK_SPACE) || gc.input.isKey(KeyEvent.VK_W)) && collide(0, 1) != null) ySpd=-2.1;
+		
+		ySpd+=0.05;
+		yMove+=ySpd;
+		
+		if (collide(0, yMove) instanceof Item) ((Item)collide(0, yMove)).pickUp();
+		
+		int loop = 100;
+		while(collide(0, yMove) != null && loop-- > 0) {
+			yMove-=Math.signum(yMove)*0.1;
+			ySpd = 0;
+		}
+		
+		if (collide(xMove, 0) instanceof Item) ((Item)collide(xMove, 0)).pickUp();
+		
+		loop = 100;
+		while((collide(xMove, 0) != null || x+xMove < 0) && loop-- > 0) xMove-=Math.signum(xMove)*0.1;
+		x+=xMove;
+		y+=yMove;
+		
+		for (int i = -1; i <= 1; i++)
+			for (int j = -1; j <= 1; j++)
+				if (collide(i, j) instanceof Item) ((Item)collide(i, j)).pickUp();
+	}
+
+	@Override
+	public void render(Graphic graphic) {
+		graphic.drawRectangle(0, 0, width, height, 0, new Color(0xffab1200));
+	}
+
+}
