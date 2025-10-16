b6149e2b1a427d2aeb3e0e54029919d747a485e5
klimdanick <github@klimdanick.nl>
2025-10-16 13:23:47 +0200

    added hitbox debug toggle

--- src/main/java/nl/klimdanick/E2/Core/Scenes/Scene.java
+++ src/main/java/nl/klimdanick/E2/Core/Scenes/Scene.java
@@ -2,11 +2,15 @@ package nl.klimdanick.E2.Core.Scenes;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import nl.klimdanick.E2.Core.Input;
 import nl.klimdanick.E2.Core.Rendering.Renderer;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_F4;
 
 public abstract class Scene {
 	
 	public CopyOnWriteArrayList<GameObject> objs = new CopyOnWriteArrayList<>();
+	public boolean drawHitboxes = false;
 	
 	public void addObject(GameObject obj) {
 		objs.add(obj);
@@ -14,6 +18,8 @@ public abstract class Scene {
 	}
 	
 	public void update_(double dt) {
+		if (Input.isKeyPressed(GLFW_KEY_F4)) drawHitboxes = !drawHitboxes;
+		
 		for (GameObject obj : objs) {
 			obj.update_(dt);
 		}
@@ -24,7 +30,7 @@ public abstract class Scene {
 		preRender(r);
 		for (GameObject obj : objs) {
 			obj.render_(r);
-			obj.hitbox.render(r);
+			if (drawHitboxes) obj.hitbox.render(r);
 		}
 		postRender(r);
 		
